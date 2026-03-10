60970eae2636e81b010dbe50a19594a90c151691
K S Keerthana <ksk66117@gmail.com>
2026-03-10 23:19:24 +0530

    Personal portfolio website built using HTML, CSS, and JavaScript

---
+++ Portfolio/script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+alert("Welcome to my Portfolio!");
+}
+var typed = new Typed(".typing", {
+strings: [
+"Computer Science Student",
+"Web Developer",
+"Problem Solver",
+"Future Software Engineer"
+],
+typeSpeed: 100,
+backSpeed: 60,
+loop: true
+});
